14158c557f00ecc71e2031454473943ca45f1079
Adarsh Gupta <adarshguptamaurya@gmail.com>
2022-10-31 02:56:15 -0400

    Created metadata extration function
    This function can further help increase the efficiency of the code

--- newFastCodesForPI/client.py
+++ newFastCodesForPI/client.py
@@ -9,6 +9,27 @@ footage_socket.connect('tcp://192.168.43.254:5555')
 timeout_start = time.time()
 camera = cv2.VideoCapture(0)  # init the camera
 
+def bulk_predict(docs, batch_size=256):
+    ''' Predict bert embeddings. '''
+    for i in range(0, len(docs), batch_size):
+        batch_docs = docs[i: i+batch_size]
+        embeddings = bc.encode([doc['text'] for doc in batch_docs])
+        for emb in embeddings:
+            yield emb
+
+def process_index_doc(id):
+    bc = BertClient(output_fmt='list')
+    client = Elasticsearch('localhost:9200')
+
+    print(f"id {type(id)}")
+    
+    db_obj = Cache.objects.get(id=id)
+    document = {}
+    document["doc_id"] = id
+    print("THIS IS THE ID ",id)
+    document["file_name"] = getVal(db_obj, "file_name")
+    document["text"] = getVal(db_obj, "text")
+
 while time.time() < timeout_start + timeout:
     try:
         (grabbed, frame) = camera.read()  # grab the current frame
