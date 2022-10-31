09fa849a5fbc750ee4d46a6da4499f2dfc14e71b
Adarsh Gupta <adarshguptamaurya@gmail.com>
2022-10-31 03:11:41 -0400

    repaired broken routes
    using latest route lib

--- register/src/Login.js
+++ register/src/Login.js
@@ -51,6 +51,33 @@ const theme = createMuiTheme({
 });
 
 class Register extends React.Component {
+  <Route
+      {...rest}
+      render={(matchProps) => (
+        <div>
+          <div
+            onClick={() => setToggleSidebar(!toggleSidebar)}
+            className="absolute top-0 left-0 text-gray-300 text-xl pl-3 cursor-pointer"
+          >
+            <GiHamburgerMenu className="mt-2 mx-2" />
+          </div>
+          <div
+            className={`${handleSidebar(
+              toggleSidebar
+            )} absolute w-64 overflow-y-scroll  top-0 h-screen shadow-md bg-gray-900 z-50 transition-all duration-200 ease-out`}
+          >
+            <Sidebar onClose={() => onCloseHandler()} />
+          </div>
+          <div
+            onClick={() => setToggleSidebar(!toggleSidebar)}
+            className={`absolute opacity-25 inset-0 ${
+              toggleSidebar ? "block" : "hidden"
+            } `}
+          ></div>
+          <Component {...matchProps} />
+        </div>
+      )}
+    />
   state = {
     screenshot: null,
     picButtonDisable: false,
